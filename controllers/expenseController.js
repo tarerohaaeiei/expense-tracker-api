2a4ef4239ae6e3c08aa70d041b324c8af7d25d35
tarerohaaeiei <79740703+tarerohaaeiei@users.noreply.github.com>
2024-10-15 09:20:24 +0700

    Update patch del

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -26,3 +26,37 @@ exports.addExpense = async (req, res) => {
     res.status(500).json({ msg: 'Server error' });
   }
 };
+
+
+exports.updateExpense = async (req, res) => {
+  const { title, amount, date, category, notes } = req.body;
+  const expenseId = req.params.id; 
+  try {
+    const expense = await Expense.findById(expenseId);
+    if (!expense) return res.status(404).json({ msg: 'Expense not found' });
+
+    if (title) expense.title = title;
+    if (amount) expense.amount = amount;
+    if (date) expense.date = date;
+    if (category) expense.category = category;
+    if (notes) expense.notes = notes;
+
+    await expense.save();
+    res.json(expense);
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
+
+
+exports.deleteExpense = async (req, res) => {
+  const expenseId = req.params.id; 
+  try {
+    const expense = await Expense.findByIdAndDelete(expenseId);
+    if (!expense) return res.status(404).json({ msg: 'Expense not found' });
+
+    res.json({ msg: 'Expense deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getExpenses, addExpense } = require('../controllers/expenseController');
+const { getExpenses, addExpense,updateExpense,deleteExpense } = require('../controllers/expenseController');
 const auth = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', auth, getExpenses);
 router.post('/', auth, addExpense);
+router.patch('/:id', auth, updateExpense);
+router.delete('/:id', auth, deleteExpense);
 
 module.exports = router;
